c2b9cf888e7055da5d4a8f95b6f5a75352f7647b
汪宇豪 <wangyuhao@wangyuhaos-MacBook.local>
2018-08-25 10:29:41 +0800

    add main

--- src/sample.h
+++ src/sample.h
@@ -1,6 +1,6 @@
 
-#ifndef GTEST_SAMPLES_SAMPLE1_H_
-#define GTEST_SAMPLES_SAMPLE1_H_
+#ifndef SAMPLE_H_
+#define SAMPLE_H_
 
 // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
 int Factorial(int n);
@@ -8,4 +8,4 @@ int Factorial(int n);
 // Returns true iff n is a prime number.
 bool IsPrime(int n);
 
-#endif  // GTEST_SAMPLES_SAMPLE1_H_
+#endif  
